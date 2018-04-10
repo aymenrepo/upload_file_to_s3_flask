fcd5c6f67e51ede709d524b0fd86c8b166559378
aymenrepo <aymensegnirepo@gmail.com>
2018-04-10 11:29:31 +0200

    setup file

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="upload files s3",
+    version="0.1",
+    url="https://github.com/aymenrepo/upload_file_to_s3_flask",
+    author="Aymen SEGNI",
+    author_email="",
+    description="Python API using Flask to upload files to S3 Buckets",
+    long_description="",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3.6'
+    ],
+)
